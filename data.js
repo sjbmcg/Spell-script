3b2e36d09314e8786c0053aa9db9ad0466cb66ad
sjbmcg <89Stephen89@gmail.com>
2023-01-05 10:23:24 +0000

    Begin reading sample code from data.js

--- data.js
+++ data.js
@@ -3,3 +3,9 @@ const starterProgram = `hero main
 spell main
   stash player = "Stephen"
   say "Logging into Endless Online as " + player
+  warp bank
+  stash gold = warp sewer
+  say "Gold found: " + gold
+  loot gold
+end
+
