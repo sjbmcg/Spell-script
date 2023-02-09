e5d4a1e917d369fe48036d086afae16bb57656d2
sjbmcg <89Stephen89@gmail.com>
2023-02-09 22:28:30 +0000

    Support stash assignments in the runtime

--- interpreter.js
+++ interpreter.js
@@ -223,3 +223,59 @@ class ExpressionParser {
 
     return this.parsePrimary();
   }
+
+  parsePrimary() {
+    const token = this.consume();
+
+    if (!token) {
+      throw new Error(`Line ${this.lineNumber}: expression ended too early.`);
+    }
+
+    if (token === "(") {
+      const expression = this.parseLogicalOr();
+
+      if (!this.match(")")) {
+        throw new Error(`Line ${this.lineNumber}: missing closing ")".`);
+      }
+
+      return expression;
+    }
+
+    if (/^"[^"]*"$/.test(token)) {
+      return { type: "literal", value: token.slice(1, -1) };
+    }
+
+    if (/^\d+(\.\d+)?$/.test(token)) {
+      return { type: "literal", value: Number(token) };
+    }
+
+    if (token === "ember" || token === "frost") {
+      return { type: "literal", value: token === "ember" };
+    }
+
+    if (ExpressionParser.isIdentifier(token)) {
+      return { type: "identifier", name: token };
+    }
+
+    throw new Error(`Line ${this.lineNumber}: unexpected token "${token}".`);
+  }
+
+  static isIdentifier(token) {
+    return /^[A-Za-z_][A-Za-z0-9_]*$/.test(token) && !ENDLESS_KEYWORDS.includes(token);
+  }
+}
+
+class EndlessParser {
+  constructor(source) {
+    this.lines = EndlessTokenizer.tokenizeSource(source);
+    this.index = 0;
+  }
+
+  parse() {
+    const program = {
+      entrySpell: "",
+      spells: [],
+      spellsByName: {}
+    };
+
+    while (!this.isAtEnd()) {
